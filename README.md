ef76bb633dc4db933688a5255ef8c64b4bbf9ae9
ob1 <ongjjbrandon@gmail.com>
2024-10-17 13:39:21 +0800

    init

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 # Tools
 * Structured Outputs, tool calling, information extraction
 * ideas in [SWARM](https://github.com/openai/swarm)
+
+* Gmail, Gcal, Slack
+
+Demo
+1. Agent fans out for information via Slack/Gmail, summarises it back to user
+2. Monitor daily some status from Jira/Notion
+3. Schedule a meeting with person X to follow up on some item
+
+Memory
+1. when user gives feedback to Mia on Search Queries should contain current company and alma mater, Mia should remember to search this the next time same workflow is fired
--- hello_open_agent.py
+++ hello_open_agent.py
@@ -65,56 +65,59 @@ class Agent(BaseModel):
     instructions: str = "You are a helpful Agent"
     tools: list = []
 
-def run_full_turn(agent, messages):
+    class Config:
+        arbitrary_types_allowed = True
 
-    num_init_messages = len(messages)
-    messages = messages.copy()
+# def run_full_turn(agent, messages):
 
-    while True:
+#     num_init_messages = len(messages)
+#     messages = messages.copy()
 
-        # turn python functions into tools and save a reverse map
-        tool_schemas = [function_to_schema(tool) for tool in agent.tools]
-        tools_map = {tool.__name__: tool for tool in agent.tools}
+#     while True:
 
-        # === 1. get openai completion ===
-        response = client.chat.completions.create(
-            model=agent.model,
-            messages=[{"role": "system", "content": agent.instructions}] + messages,
-            tools=tool_schemas or None,
-        )
-        message = response.choices[0].message
-        messages.append(message)
+#         # turn python functions into tools and save a reverse map
+#         tool_schemas = [function_to_schema(tool) for tool in agent.tools]
+#         tools_map = {tool.__name__: tool for tool in agent.tools}
 
-        if message.content:  # print assistant response
-            print("Assistant:", message.content)
+#         # === 1. get openai completion ===
+#         response = client.chat.completions.create(
+#             model=agent.model,
+#             messages=[{"role": "system", "content": agent.instructions}] + messages,
+#             tools=tool_schemas or None,
+#         )
+#         message = response.choices[0].message
+#         messages.append(message)
 
-        if not message.tool_calls:  # if finished handling tool calls, break
-            break
+#         if message.content:  # print assistant response
+#             print("Assistant:", message.content)
 
-        # === 2. handle tool calls ===
+#         if not message.tool_calls:  # if finished handling tool calls, break
+#             break
 
-        for tool_call in message.tool_calls:
-            result = execute_tool_call(tool_call, tools_map)
+#         # === 2. handle tool calls ===
 
-            result_message = {
-                "role": "tool",
-                "tool_call_id": tool_call.id,
-                "content": result,
-            }
-            messages.append(result_message)
+#         for tool_call in message.tool_calls:
+#             result = execute_tool_call(tool_call, tools_map)
 
-    # ==== 3. return new messages =====
-    return messages[num_init_messages:]
+#             result_message = {
+#                 "role": "tool",
+#                 "tool_call_id": tool_call.id,
+#                 "content": result,
+#             }
+#             messages.append(result_message)
+
+#     # ==== 3. return new messages =====
+#     return messages[num_init_messages:]
 
 
-def execute_tool_call(tool_call, tools_map):
-    name = tool_call.function.name
-    args = json.loads(tool_call.function.arguments)
+# def execute_tool_call(tool_call, tools_map):
+#     name = tool_call.function.name
+#     args = json.loads(tool_call.function.arguments)
 
-    print(f"Assistant: {name}({args})")
+#     print(f"Assistant: {name}({args})")
 
-    # call corresponding function with provided arguments
-    return tools_map[name](**args)
+#     # call corresponding function with provided arguments
+#     return tools_map[name](**args)
 
 
 
@@ -139,16 +142,217 @@ sales_assistant = Agent(
 )
 
 
+# Demonstrate Manual Handoff
+# messages = []
+# user_query = "Order a yellow Kane crocs and a pink one."
+# print("User:", user_query)
+# messages.append({"role": "user", "content": user_query})
+
+# response = run_full_turn(sales_assistant, messages) # sales assistant
+# messages.extend(response)
+
+
+# user_query = "Actually, I want a refund." # implitly refers to the last item
+# print("User:", user_query)
+# messages.append({"role": "user", "content": user_query})
+# response = run_full_turn(refund_agent, messages) # refund agent
+
+class Response(BaseModel):
+    agent: Optional[Agent]
+    messages: list
+
+refund_agent = Agent(
+    name="Refund Agent",
+    instructions="You are a refund agent. Help the user with refunds.",
+    tools=[execute_refund],
+)
+
+def transfer_to_refunds():
+    return refund_agent
+
+sales_assistant = Agent(
+    name="Sales Assistant",
+    instructions="You are a sales assistant. Sell the user a product.",
+    tools=[place_order],
+)
+
+def run_full_turn(agent, messages):
+    current_agent = agent
+    num_init_messages = len(messages)
+    messages = messages.copy()
+
+    while True:
+        # turn python functions into tools and save a reverse map
+        tool_schemas = [function_to_schema(tool) for tool in current_agent.tools]
+        tools = {tool.__name__: tool for tool in current_agent.tools}
+
+        # === 1. get openai completion ===
+        response = client.chat.completions.create(
+            model=current_agent.model,
+            messages=[{"role": "system", "content": current_agent.instructions}]
+            + messages,
+            tools=tool_schemas or None,
+        )
+        message = response.choices[0].message
+        messages.append(message)
+
+        if message.content:  # print agent response
+            print(f"{current_agent.name}:", message.content)
+
+        if not message.tool_calls:  # if finished handling tool calls, break
+            break
+
+        # === 2. handle tool calls ===
+        for tool_call in message.tool_calls:
+            result = execute_tool_call(tool_call, tools, current_agent.name)
+
+            if isinstance(result, Agent):  # if agent transfer, update current agent
+                current_agent = result
+                result = f"Transfered to {current_agent.name}. Adopt persona immediately."
+
+            result_message = {
+                "role": "tool",
+                "tool_call_id": tool_call.id,
+                "content": result,
+            }
+            messages.append(result_message)
+
+    # ==== 3. return last agent used and new messages =====
+    return Response(agent=current_agent.dict(), messages=messages[num_init_messages:])
+
+
+def execute_tool_call(tool_call, tools, agent_name):
+    name = tool_call.function.name
+    args = json.loads(tool_call.function.arguments)
+
+    print(f"{agent_name}:", f"{name}({args})")
+
+    return tools[name](**args)  # call corresponding function with provided arguments
+
+# messages = []
+# user_query = "Order a yellow Kane crocs."
+# print("User:", user_query)
+# messages.append({"role": "user", "content": user_query})
+
+# response = run_full_turn(sales_assistant, messages) # sales assistant
+# messages.extend(response)
+
+
+# user_query = "Actually, I want a refund." # implitly refers to the last item
+# print("User:", user_query)
+# messages.append({"role": "user", "content": user_query})
+# response = run_full_turn(refund_agent, messages) # refund agent
+
+def escalate_to_human(summary):
+    """Only call this if explicitly asked to."""
+    print("Escalating to human agent...")
+    print("\n=== Escalation Report ===")
+    print(f"Summary: {summary}")
+    print("=========================\n")
+    exit()
+
+
+def transfer_to_sales_agent():
+    """User for anything sales or buying related."""
+    return sales_agent
+
+
+def transfer_to_issues_and_repairs():
+    """User for issues, repairs, or refunds."""
+    return issues_and_repairs_agent
+
+
+def transfer_back_to_triage():
+    """Call this if the user brings up a topic outside of your purview,
+    including escalating to human."""
+    return triage_agent
+
+
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions=(
+        "You are a customer service bot for ACME Inc. "
+        "Introduce yourself. Always be very brief. "
+        "Gather information to direct the customer to the right department. "
+        "But make your questions subtle and natural."
+    ),
+    tools=[transfer_to_sales_agent, transfer_to_issues_and_repairs, escalate_to_human],
+)
+
+
+def execute_order(product, price: int):
+    """Price should be in USD."""
+    print("\n\n=== Order Summary ===")
+    print(f"Product: {product}")
+    print(f"Price: ${price}")
+    print("=================\n")
+    confirm = input("Confirm order? y/n: ").strip().lower()
+    if confirm == "y":
+        print("Order execution successful!")
+        return "Success"
+    else:
+        print("Order cancelled!")
+        return "User cancelled order."
+
+
+sales_agent = Agent(
+    name="Sales Agent",
+    instructions=(
+        "You are a sales agent for ACME Inc."
+        "Always answer in a sentence or less."
+        "Follow the following routine with the user:"
+        "1. Ask them about any problems in their life related to catching roadrunners.\n"
+        "2. Casually mention one of ACME's crazy made-up products can help.\n"
+        " - Don't mention price.\n"
+        "3. Once the user is bought in, drop a ridiculous price.\n"
+        "4. Only after everything, and if the user says yes, "
+        "tell them a crazy caveat and execute their order.\n"
+        ""
+    ),
+    tools=[execute_order, transfer_back_to_triage],
+)
+
+
+def look_up_item(search_query):
+    """Use to find item ID.
+    Search query can be a description or keywords."""
+    item_id = "item_132612938"
+    print("Found item:", item_id)
+    return item_id
+
+
+def execute_refund(item_id, reason="not provided"):
+    print("\n\n=== Refund Summary ===")
+    print(f"Item ID: {item_id}")
+    print(f"Reason: {reason}")
+    print("=================\n")
+    print("Refund execution successful!")
+    return "success"
+
+
+issues_and_repairs_agent = Agent(
+    name="Issues and Repairs Agent",
+    instructions=(
+        "You are a customer support agent for ACME Inc."
+        "Always answer in a sentence or less."
+        "Follow the following routine with the user:"
+        "1. First, ask probing questions and understand the user's problem deeper.\n"
+        " - unless the user has already provided a reason.\n"
+        "2. Propose a fix (make one up).\n"
+        "3. ONLY if not satesfied, offer a refund.\n"
+        "4. If accepted, search for the ID and then execute refund."
+        ""
+    ),
+    tools=[execute_refund, look_up_item, transfer_back_to_triage],
+)
+
+agent = triage_agent
 messages = []
-user_query = "Order a yellow Kane crocs and a pink one."
-print("User:", user_query)
-messages.append({"role": "user", "content": user_query})
 
-response = run_full_turn(sales_assistant, messages) # sales assistant
-messages.extend(response)
+while True:
+    user = input("User: ")
+    messages.append({"role": "user", "content": user})
 
-
-user_query = "Actually, I want a refund." # implitly refers to the last item
-print("User:", user_query)
-messages.append({"role": "user", "content": user_query})
-response = run_full_turn(refund_agent, messages) # refund agent
+    response = run_full_turn(agent, messages)
+    agent = response.agent
+    messages.extend(response.messages)
